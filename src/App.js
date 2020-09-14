c5146c43884a2e5674936921fdd19193b34ed4e6
金子純樹 <kanekojunki@kanekojunjunoMacBook-Air.local>
2020-09-14 16:23:39 +0900

    画面切替

--- src/App.js
+++ src/App.js
@@ -1,77 +1,43 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
 
-import Posts from './Posts.js';
-import PostForm from './PostForm.js';
+import AppPost from './Posts/AppPost.js';
+import AppUser from './Users/AppUser.js';
 
 export default class App extends Component {
     constructor() {
-        super();
+        super()
 
         this.state = {
-            posts: [],
-            newPost: {
-                name: '',
-                content: '',
-            }
+            user: null
         }
-
-        this.addPost = this.addPost.bind(this)
-        this.deletePost = this.deletePost.bind(this)
+        
+        this.loginRequest = this.loginRequest.bind(this)
+        this.logoutRequest = this.logoutRequest.bind(this)
     }
 
-    componentDidMount() {
-        axios
-            .get('http://laravel-react.laravel/api/post')
-            .then(response => {
-                this.setState({posts: response.data});
-            })
-            .catch(() => {
-                console.log('通信に失敗しました');
-            });
+    loginRequest() {
+        this.setState({user: {
+            user: {
+                email: 'test@example.com', 
+                name: 'user name', 
+                token: 'token'
+            }
+        }})
     }
 
-    addPost(e, newPost) {
-        e.preventDefault();
-
-        // 記事作成リクエスト
-        axios
-            .post('http://laravel-react.laravel/api/post/add', {'new_post': newPost})
-            .then(response => {
-                this.setState({posts: this.state.posts.concat({
-                    id: response['data']['id'],
-                    name: response['data']['name'],
-                    content: response['data']['content']
-                })})
-                this.setState({newPost: {name: '', content: ''}});
-            })
-            .catch(() => {
-                console.log('通信に失敗しました');
-            });
-    }
-
-    deletePost(id) {
-        axios
-            .post('http://laravel-react.laravel/api/post/delete', {'post_id': id})
-            .then(response => {
-                this.setState({posts: this.state.posts.filter(post => post.id !== response['data']['id'])})
-            })
-            .catch((e) => {
-                console.log('通信に失敗しました');
-                console.log(e);
-            });
+    logoutRequest() {
+        this.setState({user: null})
     }
 
     render() {
         return (
             <div>
-                <PostForm addPost={this.addPost}></PostForm>
-                <ul>
-                    {this.state.posts.map(post => (
-                        <Posts key={post.id} post={post} deletePost={this.deletePost}></Posts>
-                    ))}
-                </ul>
+                {this.state.user === null 
+                    ? <AppUser loginRequest={this.loginRequest}></AppUser>
+                    : <AppPost></AppPost>
+                }
+                <button onClick={this.logoutRequest}>ログアウト</button>
             </div>
         )
     }
---
+++ src/Posts/AppPost.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import Posts from './Posts.js';
+import PostForm from './PostForm.js';
+
+export default class AppPost extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            posts: [],
+            newPost: {
+                name: '',
+                content: '',
+            }
+        }
+
+        this.addPost = this.addPost.bind(this)
+        this.deletePost = this.deletePost.bind(this)
+    }
+
+    componentDidMount() {
+        axios
+            .get(process.env.REACT_APP_API_URL+'/api/post')
+            .then(response => {
+                this.setState({posts: response.data});
+            })
+            .catch(() => {
+                console.log('通信に失敗しました');
+            });
+    }
+
+    addPost(e, newPost) {
+        e.preventDefault();
+
+        // 記事作成リクエスト
+        axios
+            .post(process.env.REACT_APP_API_URL+'/api/post/add', {'new_post': newPost})
+            .then(response => {
+                this.setState({posts: this.state.posts.concat({
+                    id: response['data']['id'],
+                    name: response['data']['name'],
+                    content: response['data']['content']
+                })})
+                this.setState({newPost: {name: '', content: ''}});
+            })
+            .catch(() => {
+                console.log('通信に失敗しました');
+            });
+    }
+
+    deletePost(id) {
+        axios
+            .post(process.env.REACT_APP_API_URL+'/api/post/delete', {'post_id': id})
+            .then(response => {
+                this.setState({posts: this.state.posts.filter(post => post.id !== response['data']['id'])})
+            })
+            .catch((e) => {
+                console.log('通信に失敗しました');
+                console.log(e);
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <PostForm addPost={this.addPost}></PostForm>
+                <ul>
+                    {this.state.posts.map(post => (
+                        <Posts key={post.id} post={post} deletePost={this.deletePost}></Posts>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
